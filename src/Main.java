1885365866201ff503964880ea1d4e2cd2fdbd01
Fotorezist <error_www@mail.ru>
2024-05-21 17:31:33 +0400

    Homework for Lesson2

--- src/Main.java
+++ src/Main.java
@@ -3,29 +3,43 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
+//      Задание 1
         printThreeWords();
+//      Задание 2
         checkSumSign();
+//      Задание 3
         printColor();
+//      Задание 4
         compareNumbers();
-        System.out.println(compareSumm(11, 1));
+//      Задание 5
+        System.out.println(compareSumm(11, 11));
+//      Задание 6
         positiveOrNegative(-5);
+//      Задание 7
+        System.out.println(positiveOrNegativeBoolean(-3));
+//      Задание 8
         printString("Кря, кря", 3);
-        leapYear(2000);
+//      Задание 9
+        System.out.println(leapYear(2000));
 
+//      Задание 10
         int[] arr10 = {1,1,1,0,0,0};
         for (int i = 0; i < arr10.length; i++) {
             if (arr10[i] == 1)
                 arr10[i] = 0;
             else
                 arr10[i] = 1;
+//            System.out.println(arr10[i]);
         }
 
-
+//      Задание 11
         int[] arr11 = new int[100];
         for (int i = 0; i < arr11.length; i++) {
             arr11[i] = i + 1;
 //            System.out.println(arr11[i]);
         }
+
+//      Задание 12
         System.out.println();
         int[] arr12 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         for (int i = 0; i<arr12.length; i++) {
@@ -34,12 +48,14 @@ public class Main {
 //            System.out.println(arr12[i]);
 
         }
+
+//      Задание 13
         int[][] arr13 = new int [5][5];
         for (int i = 0; i < arr13.length; i++) {
             for (int j = 0; j < arr13.length; j++) {
                 if (i == j)
                     arr13[i][j] = 1;
-                else if ((i + j) == arr13.length-1)
+                else if ((i + j) == (arr13.length - 1))
                     arr13[i][j] =1;
 
             }
@@ -49,15 +65,20 @@ public class Main {
 //        Вывод двумерного массива
 //        Arrays.stream(arr13).map(Arrays::toString).forEach(System.out::println);
 
+//      Задание 14
         initialArray(2,55);
 
             }
+
+//      Методы
+//      Задание 1
     public static void printThreeWords() {
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
 
     }
+//    Задание 2
     public static void checkSumSign(){
         int a, b;
         a = 4;
@@ -68,9 +89,9 @@ public class Main {
             System.out.println("Сумма отрицательная");
 
     }
-
+//    Задание 3
     public static void printColor() {
-        int value = 16;
+        int value = 5;
 
         if (value <= 0)
             System.out.println("Красный");
@@ -81,6 +102,7 @@ public class Main {
 
 
     }
+//    Задание 4
     public static void compareNumbers() {
         int a, b;
         a = 1;
@@ -91,45 +113,67 @@ public class Main {
             System.out.println("a < b");
 
     }
-
+//      Задание 5
     public static boolean compareSumm(int a, int b) {
-        boolean c = (a+b) >= 10;
-        boolean d = (a+b) <= 20;
-        boolean e = c==d;
+        boolean e;
+        if ((a+b) < 10)
+            e = false;
+        else if ((a+b) > 20)
+            e = false;
+        else e = true;
+
+//        Вариант без if )
+//        boolean c = (a+b) >= 10;
+//        boolean d = (a+b) <= 20;
+//        boolean e = c==d;
+
         return e;
 
     }
-
-    public static void positiveOrNegative(int a) {
-        if (a >= 0)
+//      Задание 6
+    public static void positiveOrNegative(int number) {
+        if (number >= 0)
             System.out.println("Число положительное");
         else
             System.out.println("Число отрицательное");
 
     }
-
-    public static void printString(String a, int b) {
-        for (int i = 0; i < b; i++) {
-            System.out.println(a);
+//      Задание 7
+    public static boolean positiveOrNegativeBoolean(int number) {
+        boolean e;
+        if (number < 0)
+            e = true;
+        else e = false;
+        return e;
+    }
+//      Задание 8
+    public static void printString(String line, int amount) {
+        for (int i = 0; i < amount; i++) {
+            System.out.println(line);
         }
 
     }
+//      Задание 9
+    public static boolean leapYear(int year) {
+        boolean leapYear;
+        if (year % 400 == 0) {
+            leapYear = true;
 
-    public static void leapYear(int year) {
-    if (year % 400 == 0) {
-        System.out.println("Год високосный");
         } else if (year % 100 == 0) {
-            System.out.println("Год не високосный");
+            leapYear = false;
 
             } else if (year % 4 == 0) {
-            System.out.println("Год високосный");
+            leapYear = true;
 
-        } else {System.out.println("Год не високосный");
+        } else {
+            leapYear = false;
+
+    }
+        return leapYear;
 
     }
 
-    }
-
+//      Задание 14
     public static void initialArray(int len, int initialValue) {
         int[] arr14 = new int[len];
         for (int i = 0; i < len; i++) {
@@ -138,5 +182,6 @@ public class Main {
         }
 
     }
+
 
 }
