b55facc70a2aa1b90c71d9906c0cb503e56d6ad3
Fotorezist <error_www@mail.ru>
2024-05-24 18:03:05 +0400

    Homework for Lesson3 v2

--- src/Employer.java
+++ src/Employer.java
@@ -1,12 +1,21 @@
 public class Employer {
-    public String middleName;
-    public String name;
-    public String surName;
-    public String email;
-    public String phone;
-    public int salary;
-    public int age;
-public Employer (){
+    private String middleName;
+    private String name;
+    private String surName;
+    private String email;
+    private String phone;
+    private int salary;
+    private int age;
+    public Employer (String middleName, String name, String surName, String email, String phone, int salary, int age){
+        this.middleName = middleName;
+        this.name = name;
+        this.surName = surName;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+    public Employer (){
         this.middleName = "Иванов";
         this.name = "Иван";
         this.surName = "Иванович";
@@ -16,7 +25,13 @@ public Employer (){
         this.age = 30;
 
     }
-    public void info() {
+    public void personInfo() {
         System.out.println("Данные сотрудника: \n" + " ФИО: " + middleName + " " + name + " " + surName + "\n email: " + email + "\n Телефон: " + phone + "\n Зарплата: " + salary + "\n Возраст: " + age);
     }
+
+//    Вывод данных сотрудников из массива
+    public String toString() {
+        return "Данные сотрудника: " + " ФИО: " + middleName + " " + name + " " + surName + " email: " + email + " Телефон: " + phone + " Зарплата: " + salary + " Возраст: " + age;
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,26 @@
+
+
 public class Main {
+
     public static void main(String[] args) {
         Employer employer1;
         employer1 = new Employer();
-//        employer1.name = "Name";
-//        System.out.println(employer1.name);
-        Employer.info();
-    }
+        employer1.personInfo();
+        System.out.println();
+
+        Employer[] emplArr = new Employer[5];
+        emplArr [0] = new Employer("Сидоров", "Семён", "Семёнович", "email1@email.ru", "+11111111111", 100, 10);
+        emplArr [1] = new Employer("Петров", "Пётр", "Петрович", "email2@email.ru", "+22222222222", 200, 20);
+        emplArr [2] = new Employer("Кузнецов", "Андрей", "Дмитриевич", "email3@email.ru", "+33333333333", 300, 30);
+        emplArr [3] = new Employer("Галицкий", "Генадий", "Александрович", "email4@email.ru", "+44444444444", 400, 40);
+        emplArr [4] = new Employer("Дмитриевский", "Евгений", "Олегович", "email5@email.ru", "+55555555555", 500, 50);
+
+        //    Вывод данных сотрудников из массива
+        for (int i = 0; i< emplArr.length; i++) {
+            System.out.println(emplArr[i]);
+        }
+
+
+        }
+
 }
