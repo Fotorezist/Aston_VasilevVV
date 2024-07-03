11c75436559d453eec01852e2c46b842ca549f90
Fotorezist <error_www@mail.ru>
2024-07-03 17:20:57 +0400

    Homework for Lesson14 allTests v2

--- Test/FactorialTestJunit5.java
+++ Test/FactorialTestJunit5.java
@@ -1,8 +1,8 @@
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
-
 import static org.junit.jupiter.api.Assertions.*;
+
 @TestMethodOrder(OrderAnnotation.class)
 
 public class FactorialTestJunit5 {
@@ -17,8 +17,8 @@ public class FactorialTestJunit5 {
     @Tag("Correct_values")
     @DisplayName("Проверка вычисления факториала от 0")
     public void testFactorialOfZero() {
-        Factorial factorial = new Factorial();
-        assertEquals(1, factorial.calculateFactorial(0));
+//        Factorial factorial = new Factorial();
+        assertEquals(1, Factorial.calculateFactorial(0));
         System.out.println("Тест 1 пройден успешно");
     }
 
@@ -27,8 +27,8 @@ public class FactorialTestJunit5 {
     @Tag("Correct_values")
     @DisplayName("Проверка вычисления факториала от 1")
     public void testFactorialOfOne() {
-        Factorial factorial = new Factorial();
-        assertEquals(1, factorial.calculateFactorial(1));
+//        Factorial factorial = new Factorial();
+        assertEquals(1, Factorial.calculateFactorial(1));
         System.out.println("Тест 2 пройден успешно");
     }
 
@@ -37,8 +37,8 @@ public class FactorialTestJunit5 {
     @Tag("Correct_values")
     @DisplayName("Проверка вычисления факториала от 2")
     public void testFactorialOfTwo() {
-        Factorial factorial = new Factorial();
-        assertEquals(2, factorial.calculateFactorial(2));
+//        Factorial factorial = new Factorial();
+        assertEquals(2, Factorial.calculateFactorial(2));
         System.out.println("Тест 3 пройден успешно");
     }
 
@@ -48,8 +48,8 @@ public class FactorialTestJunit5 {
     @Tag("Correct_values")
     @DisplayName("Проверка вычисления факториала от 44(int to long)")
     public void testFactorialForLong() {
-        Factorial factorial = new Factorial();
-        assertEquals(2673996885588443136L, factorial.calculateFactorial(44));
+//        Factorial factorial = new Factorial();
+        assertEquals(2673996885588443136L, Factorial.calculateFactorial(44));
         System.out.println("Тест 4 пройден успешно");
     }
 
@@ -57,9 +57,9 @@ public class FactorialTestJunit5 {
     @Order(5)
     @DisplayName("Проверка вычисления факториала для отрицательных чисел")
     public void testFactorialOfNegativeNumber() {
-        Factorial factorial = new Factorial();
+//        Factorial factorial = new Factorial();
         Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-            factorial.calculateFactorial(-3);
+            Factorial.calculateFactorial(-3);
         });
         assertEquals("Факториал не может быть рассчитан для отрицательных чисел", exception.getMessage());
         System.out.println("Тест 5 пройден успешно");
