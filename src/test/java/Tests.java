4ac727f4b3ba32ba96497768b7a38b17acd83ef3
Fotorezist <error_www@mail.ru>
2024-07-10 16:35:45 +0400

    Homework for Lesson 16 v1

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -28,8 +28,8 @@ public class Tests {
         cookieAgreeButton.click();
     }
 
-    @Test
-    public void testPlaceholderTexts() {
+    @Test(description="Проверка плейсхолдеров в разделе 'Услуги связи'", priority = 1)
+    public void testPlaceholderTexts1() {
 
 //        Выбираем "Услуги связи" из выпадающего списка
         WebElement dropdownList = driver.findElement(By.xpath("//span[@class='select__now']"));
@@ -44,6 +44,66 @@ public class Tests {
 
 //        Сравниваем текст placeholder с ожидаемым
         Assert.assertEquals(phoneNumberPlaceholder, "Номер телефона", "Текст плейсхолдера для номера телефона некорректен.");
+        Assert.assertEquals(amountPlaceholder, "Сумма", "Текст плейсхолдера для суммы некорректен.");
+        Assert.assertEquals(emailPlaceholder, "E-mail для отправки чека", "Текст плейсхолдера для E-mail некорректен.");
+    }
+
+    @Test(description="Проверка плейсхолдеров в разделе 'Домашний интернет'", priority = 2)
+    public void testPlaceholderTexts2() {
+
+//        Выбираем "Услуги связи" из выпадающего списка
+        WebElement dropdownList = driver.findElement(By.xpath("//span[@class='select__now']"));
+        dropdownList.click();
+        WebElement listOption = driver.findElement(By.xpath("//p[contains(text(), 'Домашний интернет')]"));
+        listOption.click();
+
+//        Находим текст placeholder для полей ввода номера телефона, суммы и E-mail
+        String phoneNumberPlaceholder=driver.findElement(By.xpath("//input[@id='internet-phone']")).getAttribute("placeholder");
+        String amountPlaceholder=driver.findElement(By.xpath("//input[@id='internet-sum']")).getAttribute("placeholder");
+        String emailPlaceholder=driver.findElement(By.xpath("//input[@id='internet-email']")).getAttribute("placeholder");
+
+//        Сравниваем текст placeholder с ожидаемым
+        Assert.assertEquals(phoneNumberPlaceholder, "Номер абонента", "Текст плейсхолдера для номера телефона некорректен.");
+        Assert.assertEquals(amountPlaceholder, "Сумма", "Текст плейсхолдера для суммы некорректен.");
+        Assert.assertEquals(emailPlaceholder, "E-mail для отправки чека", "Текст плейсхолдера для E-mail некорректен.");
+    }
+
+    @Test(description="Проверка плейсхолдеров в разделе 'Рассрочка'", priority = 3)
+    public void testPlaceholderTexts3() {
+
+//        Выбираем "Услуги связи" из выпадающего списка
+        WebElement dropdownList = driver.findElement(By.xpath("//span[@class='select__now']"));
+        dropdownList.click();
+        WebElement listOption = driver.findElement(By.xpath("//p[contains(text(), 'Рассрочка')]"));
+        listOption.click();
+
+//        Находим текст placeholder для полей ввода номера телефона, суммы и E-mail
+        String phoneNumberPlaceholder=driver.findElement(By.xpath("//input[@id='score-instalment']")).getAttribute("placeholder");
+        String amountPlaceholder=driver.findElement(By.xpath("//input[@id='instalment-sum']")).getAttribute("placeholder");
+        String emailPlaceholder=driver.findElement(By.xpath("//input[@id='instalment-email']")).getAttribute("placeholder");
+
+//        Сравниваем текст placeholder с ожидаемым
+        Assert.assertEquals(phoneNumberPlaceholder, "Номер счета на 44", "Текст плейсхолдера для номера телефона некорректен.");
+        Assert.assertEquals(amountPlaceholder, "Сумма", "Текст плейсхолдера для суммы некорректен.");
+        Assert.assertEquals(emailPlaceholder, "E-mail для отправки чека", "Текст плейсхолдера для E-mail некорректен.");
+    }
+
+    @Test(description="Проверка плейсхолдеров в разделе 'Задолженность'", priority = 3)
+    public void testPlaceholderTexts4() {
+
+//        Выбираем "Услуги связи" из выпадающего списка
+        WebElement dropdownList = driver.findElement(By.xpath("//span[@class='select__now']"));
+        dropdownList.click();
+        WebElement listOption = driver.findElement(By.xpath("//p[contains(text(), 'Задолженность')]"));
+        listOption.click();
+
+//        Находим текст placeholder для полей ввода номера телефона, суммы и E-mail
+        String phoneNumberPlaceholder=driver.findElement(By.xpath("//input[@id='score-arrears']")).getAttribute("placeholder");
+        String amountPlaceholder=driver.findElement(By.xpath("//input[@id='arrears-sum']")).getAttribute("placeholder");
+        String emailPlaceholder=driver.findElement(By.xpath("//input[@id='arrears-email']")).getAttribute("placeholder");
+
+//        Сравниваем текст placeholder с ожидаемым
+        Assert.assertEquals(phoneNumberPlaceholder, "Номер счета на 2073", "Текст плейсхолдера для номера телефона некорректен.");
         Assert.assertEquals(amountPlaceholder, "Сумма", "Текст плейсхолдера для суммы некорректен.");
         Assert.assertEquals(emailPlaceholder, "E-mail для отправки чека", "Текст плейсхолдера для E-mail некорректен.");
     }
