ff6e6d4ce1522c98f1ebb825b2c2cfab172b6309
viktor <zarvanakarana@gmail.com>
2024-10-10 16:09:35 +0300

    Ветка для LiveCoding

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Map<Integer, String> originalMap = new HashMap<>();
+        originalMap.put(1, "one");
+        originalMap.put(2, "two");
+        originalMap.put(3, "three");
+
+        Map<String, Integer> swappedMap = swapKeyValue(originalMap);
+
+        System.out.println(swappedMap);
+    }
+
+    public static Map<String, Integer> swapKeyValue(Map<Integer, String> originalMap) {
+        Map<String, Integer> swappedMap = new HashMap<>();
+
+        for (Map.Entry<Integer, String> entry : originalMap.entrySet()) {
+            swappedMap.put(entry.getValue(), entry.getKey());
+        }
+
+        return swappedMap;
     }
 }
